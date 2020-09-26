2e1db780569115d6b0e64ca173a5246d6a837719
kalemai <princeivo@gmail.com>
2020-09-26 15:03:32 +0300

    Completed

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Album } from '../album';
+
+import { ProductService } from '../product.service';
+
+import { Subscriber } from 'rxjs/Subscriber';
+
+@Component({
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+
+  constructor(private _productService: ProductService) { }
+
+  albumInfo: Album;
+
+  ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
+  }
+
+}
